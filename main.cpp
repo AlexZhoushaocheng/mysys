b06b951e7339efdb86267cb8939556f8043f3052
sczhou <sczhou@robvision.com>
2022-05-16 11:26:31 +0800

    push test

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-05-16
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+  printf("hello world\n");
+
+  return 0;
+}
---
+++ mysys/src/log/logger.h
@@ -0,0 +1,14 @@
+/**
+ * @file logger.h
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-05-16
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <fmt/printf.h>
+
+namespace mysys {}
